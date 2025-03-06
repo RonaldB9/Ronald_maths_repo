08ee10a0d64323e4fd70b75fdddd80cb93bac974
RonaldB9 <rbrock@carthage.edu>
2025-03-06 14:05:37 -0600

    Added square function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void square(int input);
+
 int main()
 {
     cout<<"GitHub Test"<<endl;
+    square(2);
     return 0;
+}
+
+void square(int input) {
+    cout << input*2 << endl;
 }
